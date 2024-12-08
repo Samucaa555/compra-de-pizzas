bbafd282f717f2973caad36f845d937f8e9faf14
Samucaa555 <151879966+Samucaa555@users.noreply.github.com>
2024-12-08 11:33:03 -0300

    Update pizzas.js
    novo commit

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
     {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Uma deliciosa combinação de queijo mussarela derretido com molho de tomate especial e orégano'},
     {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Fatias de calabresa finamente cortadas, cebola caramelizada, molho de tomate e orégano'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Uma mistura irresistível de mussarela, parmesão, gorgonzola e catupiry sobre uma base de molho especial.'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Molho de tomate, queijo mussarela, presunto, milho verde e orégano.'},
-    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Massa crocante com cobertura de sorvete de creme, pedaços de chocolate e calda de morango.'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Uma receita exclusiva com mussarela, calabresa, bacon, tomate, azeitonas e um toque especial do chef.'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Massa doce com generosa cobertura de chocolate ao leite e confeitos de M&Ms.'}
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Uma mistura irresistível de mussarela, parmesão, gorgonzola e catupiry sobre uma base de molho especial'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Molho de tomate, queijo mussarela, presunto, milho verde e orégano'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Massa crocante com cobertura de sorvete de creme, pedaços de chocolate e calda de morango'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Uma receita exclusiva com mussarela, calabresa, bacon, tomate, azeitonas e um toque especial do chef'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Massa doce com generosa cobertura de chocolate ao leite e confeitos de M&Ms'}
 ];
